3009d92d498829febf8f440c3a15e468342fd58b
ekate_riiina <ekaterinabalasova405@gmail.com>
2025-06-11 04:43:27 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KP_Balashova_
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppDomain.CurrentDomain.UnhandledException += (s, ex) => {
+                Debug.WriteLine($"Unhandled exception: {ex.ExceptionObject}");
+            };
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using KP_Balashova_.ApplicationData;
+using KP_Balashova_.PageMain;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KP_Balashova_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            // Инициализация фрейма
+            AppFrame.FrameMain = FrmMain;
+            AppFrame.FrameMain.Navigate(new PageMenu(null));
+            
+        }
+
+
+        private void UpdateCartBadge()
+        {
+            // Обновляем UI (например, TextBlock с количеством товаров)
+            Dispatcher.Invoke(() =>
+            {
+                // Здесь код обновления плюсика/значка корзины
+            });
+        }
+
+        private void CategoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.FrameMain.Navigate(new PageMenu((sender as Button).Content.ToString()));
+        }
+
+        private void Btn_Korzina_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.FrameMain.Navigate(new PageKorzina());
+        }
+
+        private void Btn_Sotrudnik_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Сейчас подойдёт к вам сотрудник кафе для помощи!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Btn_Admin_Click_1(object sender, RoutedEventArgs e)
+        {
+            AppFrame.FrameMain.Navigate(new PageAdmin());
+        }
+    }
+}
